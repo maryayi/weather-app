4b2c6cd03ac5543cf945fe8fcb8e8b3d5a617cba
Mahdi Aryayi <mahdiaryayi@gmail.com>
2021-10-12 15:43:05 +0330

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
+const WeatherBox = ({ weather = null }: { weather: any }) => {
+  if (!weather) return null;
+  if (weather?.message) return (
+    <div className="weather-box__error">
+      {weather.message}
+    </div >
+  )
+  return (
+    <div className='weather-box'>
+      <img src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} className='weather-box__icon' />
+      <p className="weather-box__description">
+        {weather.weather[0].description}
+      </p>
+      <table>
+        <tr>
+          <th>temp</th>
+          <td>{weather.main.temp}&#8451;</td>
+        </tr>
+        <tr>
+          <th>temp max</th>
+          <td>{weather.main.temp_max}&#8451;</td>
+        </tr>
+        <tr>
+          <th>temp min</th>
+          <td>{weather.main.temp_min}&#8451;</td>
+        </tr>
+      </table>
+    </div>
+  )
+};
+
+function useWeather(initialState: any = null): [any, (cityName: string) => any] {
+  const [weather, setWeather] = useState<any>(initialState);
+
+  const fetchWeather = async (cityName: string) => {
+    let result = null;
+    result = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&appid=${process.env.REACT_APP_WEATHER_API_KEY}`
+    )
+      .then(resp => resp.json());
+    setWeather(result);
+  }
+
+  return [weather, fetchWeather]
+}
+
 function App() {
+  const [weather, fetchWeather] = useWeather();
+  const [cityName, setCityName] = useState('');
+
+  useEffect(() => {
+    fetchWeather(cityName);
+  }, [cityName]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <WeatherBox weather={weather} />
+      <input type="text" value={cityName} onChange={e => setCityName(e.target.value)} />
     </div>
   );
 }
