6efa8835514f0ef0ff531c0a8106a987c5a3f5c3
Mahdi Aryayi <mahdiaryayi@gmail.com>
2021-10-12 16:36:23 +0330

    Added styling and some refactoring

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState, useCallback } from 'react';
 import './App.css';
+import { useWeather } from './hooks'
+import { debounce } from './helpers'
 
-const WeatherBox = ({ weather = null }: { weather: any }) => {
+interface IWeatherBox {
+  weather: any,
+  loading: boolean,
+}
+
+const WeatherBox = ({ weather = null, loading = false }: IWeatherBox) => {
+  if (loading) return (
+    <div className="weather-box__error">
+      Loading...
+    </div >
+  )
   if (!weather) return null;
   if (weather?.message) return (
     <div className="weather-box__error">
@@ -10,54 +22,54 @@ const WeatherBox = ({ weather = null }: { weather: any }) => {
   )
   return (
     <div className='weather-box'>
-      <img src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} className='weather-box__icon' />
-      <p className="weather-box__description">
-        {weather.weather[0].description}
-      </p>
-      <table>
+      <h2 className="weather-box__city">
+        {weather.name}, {weather.sys.country}
+      </h2>
+      <div className="weather-box__result">
+        <img src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} className='weather-box__icon' />
+        <p className="weather-box__description">
+          {weather.weather[0].description}
+        </p>
+      </div>
+      <table className="weather-box__table">
+        <tr>
+          <th className="weather-box__table--cold">temp min</th>
+          <td>{weather.main.temp_min}&#8451;</td>
+        </tr>
         <tr>
           <th>temp</th>
           <td>{weather.main.temp}&#8451;</td>
         </tr>
         <tr>
-          <th>temp max</th>
+          <th className="weather-box__table--hot">temp max</th>
           <td>{weather.main.temp_max}&#8451;</td>
-        </tr>
-        <tr>
-          <th>temp min</th>
-          <td>{weather.main.temp_min}&#8451;</td>
         </tr>
       </table>
     </div>
   )
 };
 
-function useWeather(initialState: any = null): [any, (cityName: string) => any] {
-  const [weather, setWeather] = useState<any>(initialState);
 
-  const fetchWeather = async (cityName: string) => {
-    let result = null;
-    result = await fetch(
-      `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&appid=${process.env.REACT_APP_WEATHER_API_KEY}`
-    )
-      .then(resp => resp.json());
-    setWeather(result);
-  }
-
-  return [weather, fetchWeather]
-}
 
 function App() {
-  const [weather, fetchWeather] = useWeather();
+  const [weather, fetchWeather, loading] = useWeather();
   const [cityName, setCityName] = useState('');
 
+  const debouncedFetchWeather = useCallback(debounce(fetchWeather, 500), []);
   useEffect(() => {
-    fetchWeather(cityName);
+    debouncedFetchWeather(cityName);
   }, [cityName]);
   return (
     <div className="App">
-      <WeatherBox weather={weather} />
-      <input type="text" value={cityName} onChange={e => setCityName(e.target.value)} />
+      <h1 className="weather__title">Find weather of any city</h1>
+      <input
+        placeholder="Enter city name..."
+        className="weather__input"
+        type="text"
+        value={cityName}
+        onChange={e => setCityName(e.target.value)}
+      />
+      <WeatherBox weather={weather} loading={loading} />
     </div>
   );
 }
